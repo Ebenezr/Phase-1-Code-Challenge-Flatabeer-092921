10b336900f9c04aaba29383115e97a19476129e7
Ebenezr <ebenezarbukosia@gmail.com>
2022-06-18 10:23:33 +0300

    add fetch get funtion

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 // Code here
+document.addEventListener("DOMContentLoaded", () => {
+  function fetchData() {
+    fetch("http://localhost:3000/beers")
+      .then((resp) => resp.json())
+      .then((data) => renderQuotes(data));
+  }
+  const delbtn = document.querySelectorAll(".btn");
+});
