59c7b294a99c86e2cbbb3830c08226840ad6055e
Ebenezr <ebenezarbukosia@gmail.com>
2022-06-18 15:16:30 +0300

    add render beer to DOM function

--- src/index.js
+++ src/index.js
@@ -3,41 +3,58 @@ document.addEventListener("DOMContentLoaded", () => {
   function fetchData() {
     fetch("http://localhost:3000/beers")
       .then((resp) => resp.json())
-      .then((data) => renderBeers(data))
+      .then((data) => renderBeers(data));
+  }
+  //select beers
+  function fetchBeer() {
+    fetch("http://localhost:3000/beers")
+      .then((resp) => resp.json())
       .then((data) => selectBeers(data));
   }
+
   //Find the container where we attach everything to
   const beerUl = document.querySelector("#beer-list");
+
   function renderBeers(data) {
-    for (const q of data) {
+    //console.log(data);
+    for (const beer of data) {
+      //console.log(beer.name);
       //Create necessary elements
       const beerLi = document.createElement("li");
-      beerLi.dataset.id = q.id;
+      beerLi.dataset.id = beer.id;
       //Grab data and insert it into created elements
       beerLi.innerHTML = `
-        <h5>${q.name}</h5>
+        <h5>${beer.name}</h5>
         `;
       beerUl.append(beerLi);
     }
   }
   function selectBeers(data) {
-    let name = document.getElementById("beer-name");
-    let image = document.getElementById("beer-image");
-    let description = document.getElementById("beer-description");
-    //click list event
+    for (const beer of data) {
+      console.log(beer.name);
 
-    beerUl.addEventListener("click", (event) => {
-      if (event.target.matches("li")) {
-        event.target.style.background = "#3493D5";
-        event.target.style.color = "#ffff";
-        event.target.style.display = "block";
-        console.log(event.target.innerText);
-        // name.innerText = data.name;
-        // image.src = data.image_url;
-        // description.innerText = data.description;
-      }
-    });
+      let name = document.getElementById("beer-name");
+      let image = document.getElementById("beer-image");
+      let description = document.getElementById("beer-description");
+      // console.log(beer.name);
+      //click list event
+
+      beerUl.addEventListener("click", (event) => {
+        if (event.target.matches("li")) {
+          event.target.style.background = "#3493D5";
+          event.target.style.color = "#ffff";
+          event.target.style.display = "block";
+
+          //console.log(event.target.innerText);
+          name.innerText = beer.name;
+          image.src = beer.image_url;
+          //console.log(data.description);
+          description.innerText = beer.description;
+        }
+      });
+    }
   }
   //Call the function that will automatically run renderBeers)
   fetchData();
+  fetchBeer();
 });
