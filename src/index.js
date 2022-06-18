3495708ea91aa2b68c9ec999239d7c36c009fb57
Ebenezr <ebenezarbukosia@gmail.com>
2022-06-18 11:09:39 +0300

    select beer function

--- src/index.js
+++ src/index.js
@@ -3,25 +3,40 @@ document.addEventListener("DOMContentLoaded", () => {
   function fetchData() {
     fetch("http://localhost:3000/beers")
       .then((resp) => resp.json())
-      .then((data) => renderBeers(data));
+      .then((data) => renderBeers(data))
+      .then((data) => selectBeers(data));
   }
-  //<img src="${q.image_url}">
-  //  <blockquote>${q.description} </blockquote>
-  //       <li>${q.reviews}</li>
-  //       <button class="btn">Learn more</button>
+  //Find the container where we attach everything to
+  const beerUl = document.querySelector("#beer-list");
   function renderBeers(data) {
     for (const q of data) {
-      //Find the container where we attach everything to
-      const quoteUL = document.querySelector("#beer-list");
       //Create necessary elements
-      const quoteLi = document.createElement("li");
-      quoteLi.dataset.id = q.id;
+      const beerLi = document.createElement("li");
+      beerLi.dataset.id = q.id;
       //Grab data and insert it into created elements
-      quoteLi.innerHTML = `
+      beerLi.innerHTML = `
         <h5>${q.name}</h5>
         `;
-      quoteUL.append(quoteLi);
+      beerUl.append(beerLi);
     }
+  }
+  function selectBeers(data) {
+    let name = document.getElementById("beer-name");
+    let image = document.getElementById("beer-image");
+    let description = document.getElementById("beer-description");
+    //click list event
+
+    beerUl.addEventListener("click", (event) => {
+      if (event.target.matches("li")) {
+        event.target.style.background = "#3493D5";
+        event.target.style.color = "#ffff";
+        event.target.style.display = "block";
+        console.log(event.target.innerText);
+        // name.innerText = data.name;
+        // image.src = data.image_url;
+        // description.innerText = data.description;
+      }
+    });
   }
   //Call the function that will automatically run renderBeers)
   fetchData();
