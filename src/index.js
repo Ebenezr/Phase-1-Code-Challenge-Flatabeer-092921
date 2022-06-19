6c26db49ab3e167f0db3351278dc4cc82d4c8327
Ebenezr <ebenezarbukosia@gmail.com>
2022-06-19 18:48:03 +0300

    fix: fix render characters to dom with click

--- src/index.js
+++ src/index.js
@@ -1,60 +1,29 @@
 // Code here
 document.addEventListener("DOMContentLoaded", () => {
-  function fetchData() {
-    fetch("http://localhost:3000/beers")
-      .then((resp) => resp.json())
-      .then((data) => renderBeers(data));
-  }
-  //select beers
-  function fetchBeer() {
-    fetch("http://localhost:3000/beers")
-      .then((resp) => resp.json())
-      .then((data) => selectBeers(data));
-  }
-
-  //Find the container where we attach everything to
-  const beerUl = document.querySelector("#beer-list");
-
-  function renderBeers(data) {
-    //console.log(data);
-    for (const beer of data) {
-      //console.log(beer.name);
-      //Create necessary elements
-      const beerLi = document.createElement("li");
-      beerLi.dataset.id = beer.id;
-      //Grab data and insert it into created elements
-      beerLi.innerHTML = `
-        <h5>${beer.name}</h5>
-        `;
-      beerUl.append(beerLi);
-    }
-  }
-  function selectBeers(data) {
-    for (const beer of data) {
-      console.log(beer.name);
-
-      let name = document.getElementById("beer-name");
-      let image = document.getElementById("beer-image");
-      let description = document.getElementById("beer-description");
-      // console.log(beer.name);
-      //click list event
-
-      beerUl.addEventListener("click", (event) => {
-        if (event.target.matches("li")) {
-          event.target.style.background = "#3493D5";
-          event.target.style.color = "#ffff";
-          event.target.style.display = "block";
-
-          //console.log(event.target.innerText);
-          name.innerText = beer.name;
-          image.src = beer.image_url;
-          //console.log(data.description);
-          description.innerText = beer.description;
-        }
-      });
-    }
-  }
   //Call the function that will automatically run renderBeers)
   fetchData();
-  fetchBeer();
 });
+//fetch beers
+function fetchData() {
+  fetch("http://localhost:3000/beers")
+    .then((resp) => resp.json())
+    .then((data) => renderBeers(data));
+}
+//Find the container where we attach everything to
+const beerUl = document.querySelector("#beer-list");
+let beerName = document.getElementById("beer-name");
+let beerImage = document.getElementById("beer-image");
+let beerDescription = document.getElementById("beer-description");
+function renderBeers(data) {
+  data.forEach((data) => {
+    const list = document.createElement("li");
+    list.innerText = data.name;
+
+    beerUl.appendChild(list);
+    list.addEventListener("click", () => {
+      beerName.textContent = data.name;
+      beerImage.setAttribute("src", data.image_url);
+      beerDescription.textContent = data.description;
+    });
+  });
+}
